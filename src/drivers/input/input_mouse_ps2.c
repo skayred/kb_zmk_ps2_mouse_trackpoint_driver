40b0abc9bbfc8dea2883a9f66aa54448732d2853
Dmitrii Savchenko <dmitrii.savchenko@mvision.ai>
2026-02-20 16:05:07 +0200

    own driver

--- src/drivers/input/input_mouse_ps2.c
+++ src/drivers/input/input_mouse_ps2.c
@@ -14,7 +14,7 @@
 #include <zephyr/drivers/ps2.h>
 #include <zephyr/dt-bindings/input/input-event-codes.h>
 #include <zephyr/input/input.h>
-#include <zephyr/logging/log.h
+#include <zephyr/logging/log.h>
 #include <zephyr/settings/settings.h>
 #include <zephyr/sys/util.h>
 #include <zmk/event_manager.h>
